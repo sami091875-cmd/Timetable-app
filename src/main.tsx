7fd59ec6b4586c84fecc99f8b1a48c21f99f27b0
sami091875-cmd <sami091875@gmail.com>
2026-07-07 02:22:06 +0300

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
+---------- Forwarded message ---------
+From: Abdullah Othman <alsaeed980o0o0o@gmail.com>
+Date: Tue, Jul 7, 2026, 1:54 AM
+Subject: File8
+To: <sami091875-cmd@gmail.com>
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
